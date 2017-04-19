faa22d6a723e3e22545dc65396152c528e909f75
CJ <cjzhang88@gmail.com>
2017-04-20 00:01:44 +0800

    使用ejs显示users路由

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+var path = require('path')
 var express = require('express')
 var app = express()
 var indexRouter = require('./routers/index')
 var userRouter = require('./routers/users')
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
 
 app.use('/',indexRouter)
 app.use('/users',userRouter)
