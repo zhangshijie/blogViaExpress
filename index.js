306b5cb049aebefa7f2de38411a75b5be6959ac2
CJ <cjzhang88@gmail.com>
2017-04-20 00:14:09 +0800

    错误处理

--- index.js
+++ index.js
@@ -12,12 +12,17 @@ app.use('/users',userRouter)
 
 app.use(function(req, res, next) {
   console.log('1');
-  next();
+  next(new Error('haha'))
 });
 
 app.use(function(req, res, next) {
   console.log('2');
   res.status(200).end();
 });
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('something broke')
+})
 
 app.listen(3000);
